0a1daab793c63e329981d8308d55aca918e0011d
mrinal-mann <mrinal.mann24@gmail.com>
2025-05-20 13:29:40 +0530

    updated the code

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBp_JPcbkcyX38Cyo-XK2dNcsX-AJ7jLSI",
   authDomain: "pixmix-6a12e.firebaseapp.com",
   projectId: "pixmix-6a12e",
   storageBucket: "pixmix-6a12e.appspot.com",
